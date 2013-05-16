31f507147ec70055d5e5f9e54fcbbbfa9f1ac5aa
Mauro S. Mackinze <mauro.mackinze@gmail.com>
2013-05-16 17:22:19 -0300

    - Improved PlayerHelper.cs.

--- Cinchcast.Btr.Android/PlayerHelper.cs
+++ Cinchcast.Btr.Android/PlayerHelper.cs
@@ -8,7 +8,7 @@ namespace Cinchcast.Btr.Android
 	{
 		MediaPlayer _player;
 		Activity _context;
-
+		string _currentUrl = null;
 
 		public PlayerHelper(Activity context)
 		{
@@ -17,17 +17,27 @@ namespace Cinchcast.Btr.Android
 
 		public void Play(string url)
 		{
-			if (_player != null)
-			{
-				_player.Pause ();
-				_player.Release ();
-				_player.Dispose ();
-			}
+			// If the url sent is the same we pause or start the current player.
+			if (_currentUrl == url) {
+				if (_player.IsPlaying) {
+					_player.Pause ();
+				} else {
+					_player.Start ();
+				}
+			} else {
+				// IF the url is not the same, then we must reset the current player and load the new url.
+				_currentUrl = url;
+				if (_player != null) {
+					_player.Pause ();
+					_player.Release ();
+					_player.Dispose ();
+				}
 
-			//Uri uri = Uri.Parse("http://www.blogtalkradio.com/craptasticallyhorrific/2013/05/16/sledgecast-death-tolls-1st-time.mp3");
-			Uri uri = Uri.Parse(url);
-			_player = MediaPlayer.Create (_context, uri);
-			_player.Start ();
+				//Uri uri = Uri.Parse("http://www.blogtalkradio.com/craptasticallyhorrific/2013/05/16/sledgecast-death-tolls-1st-time.mp3");
+				Uri uri = Uri.Parse(url);
+				_player = MediaPlayer.Create (_context, uri);
+				_player.Start ();
+			}
 		}
 	
 	}
